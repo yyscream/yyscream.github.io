46ae6737420cf869b28801b056cd75b9182adee4
yyscream <1667726759@qq.com>
2025-01-31 23:51:40 +0800

    Site updated: 2025-01-31 23:51:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/06/hello-world/","2025/01/06/第一篇文章/"];function toRandomPost(){
+var posts=["2025/01/06/第一篇文章/","2025/01/06/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
